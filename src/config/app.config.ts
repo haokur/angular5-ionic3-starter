9bd4bfc83e381ad7d2e28dd81cf918b4297ebbba
haokur <445703646@qq.com>
2018-04-23 22:03:12 +0800

    vue-cli 初始化项目结构

---
+++ src/config/app.config.ts
@@ -0,0 +1,22 @@
+export const APP_CONFIG = {
+  tabsHideOnSubPages: true,
+  activator: 'ripple',
+  tabsLayout: 'icon-top',
+  pageTransition: 'ios-transition',
+  backButtonText: 'Back',
+  swipeBackEnabled: false,
+  mode: 'ios', // 配置平台为单一平台
+  iconMode: 'ios', // 配置icon统一使用
+  modalEnter: 'modal-slide-in',
+  modalLeave: 'modal-slide-out',
+  tabsPlacement: 'bottom',
+  // locationStrategy:'hash',
+  platforms: {
+    ios: {
+
+    },
+    md: {
+
+    }
+  }
+}
